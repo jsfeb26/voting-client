3a5702bcc6fa8efd9d7582781f9d4031254416e8
jsfeb26 <jsfeb26@gmail.com>
2015-11-04 22:09:24 -0500

    Add pure render mixin and tests to test it

--- src/components/Voting.js
+++ src/components/Voting.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 import Winner from './Winner';
 import Vote from './Vote';
 
 export default React.createClass({
+  // causes react to do a shallow compare to decide when to re-render components
+  // this is a huge performance boost
+  mixins: [PureRenderMixin],
+
   render: function() {
     return <div>
       {this.props.winner?
