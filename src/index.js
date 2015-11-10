a996908db2af123e4510c94f209479fc1c9e2161
jsfeb26 <jsfeb26@gmail.com>
2015-11-10 13:42:30 -0500

    Set up socket.io on client

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Router, {Route} from 'react-router';
 import {createStore} from 'redux';
 import { Provider } from 'react-redux';
+import io from 'socket.io-client';
 import reducer from './reducer';
 import App from './components/App';
 import { VotingContainer } from './components/Voting';
@@ -18,6 +19,10 @@ store.dispatch({
     }
   }
 });
+
+// using the io function to connect ot the socket.io sdrver that's running
+// on the same hostname on port 8090
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
 
 const routes = <Route component={App}>
                 <Route path="/results" component={ResultsContainer} />
