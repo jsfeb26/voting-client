4826d13a246b90afe54dbddebb94b1e534144ef3
jsfeb26 <jsfeb26@gmail.com>
2015-11-11 22:25:21 -0500

    Update reducer and reducer test with vote functionality

--- src/reducer.js
+++ src/reducer.js
@@ -1,13 +1,26 @@
-import {Map} from 'immutable';
+import {Map, fromJS} from 'immutable';
 
 function setState(state, newState) {
   return state.merge(newState);
+}
+
+function vote(state, entry) {
+  const currentPair = state.getIn(['vote', 'pair']);
+
+  if (currentPair && currentPair.includes(entry)) {
+    return state.set('hasVoted', entry);
+  }
+  else {
+    return state;
+  }
 }
 
 export default function(state = Map(), action) {
   switch (action.type) {
     case 'SET_STATE':
       return setState(state, action.state);
+    case 'VOTE':
+      return vote(state, action.entry);
   }
 
   return state;
